ed8829ce512127edb76ff01352992d9b3d44ec0b
ABDELAZIZ MAHER <abdelazizmaher17499@gmail.com>
2022-07-20 17:14:55 +0200

    Create EX2_Check_Vowel_Or_Constant.c

---
+++ Unit2_C_Programming/Lesson3_C_Basics_Assignments/Assignment2/EX2_Check_Vowel_Or_Constant.c
@@ -0,0 +1,26 @@
+/*
+ * main.c
+ *
+ *  Created on: July 20, 2022
+ *      Author: Abdelaziz Maher
+ */
+
+#include "stdio.h"
+
+int main()
+{
+	char check;
+
+    printf("Enter an alphabet : ");
+    fflush(stdout);
+    scanf("%c",&check);
+
+    if( ( check == 'a' ) || ( check == 'e' ) || ( check == 'i' ) || ( check == 'o' ) || ( check == 'u' ) )
+    	printf("%c is a Vowel ",check);
+    else if(( check == 'A' ) || ( check == 'E' ) || ( check == 'I' ) || ( check == 'O' ) || ( check == 'U' ))
+    	printf("%c is a Vowel ",check);
+    else
+    	printf("%c is a Constant ",check);
+
+    return 0;
+}
