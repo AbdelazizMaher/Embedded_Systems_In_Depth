7b40a42a05f8da242e8596ac03b1e6ae805f1dee
ABDELAZIZ MAHER <abdelazizmaher17499@gmail.com>
2022-07-23 22:11:17 +0200

    function assignment

---
+++ Unit2_C_Programming/Lesson5_C_Functions_Assignments/EX4_Calculate_Power_Of_A_Number_Using_Recursion.c
@@ -0,0 +1,37 @@
+/*
+ * main.c
+ *
+ *  Created on: July 23, 2022
+ *      Author: Abdelaziz Maher
+ */
+
+#include "stdio.h"
+
+int calc_power(int base,int power);
+
+int main()
+{
+	int base,power;
+
+    printf("Enter base number : ");
+    fflush(stdout);
+    scanf("%d",&base);
+
+    printf("Enter power number(positive integer) : ");
+    fflush(stdout);
+    scanf("%d",&power);
+
+    // calculate values
+    printf("%d^%d = %d", base , power, calc_power(base,power) );
+
+    return 0;
+}
+
+
+int calc_power(int base,int power)
+{
+	if( power == 1)
+		return base;
+
+	return base*calc_power(base, power-1);
+}
