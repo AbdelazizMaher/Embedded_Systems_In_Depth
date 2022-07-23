5762d4b978104005ea41985203679532c2829193
ABDELAZIZ MAHER <abdelazizmaher17499@gmail.com>
2022-07-23 17:19:43 +0200

    EX2

---
+++ Unit2_C_Programming/Lesson5_C_Functions_Assignments/EX2_Calculate_Factorial_Using_Recursion.c
@@ -0,0 +1,34 @@
+/*
+ * main.c
+ *
+ *  Created on: July 23, 2022
+ *      Author: Abdelaziz Maher
+ */
+
+#include "stdio.h"
+
+
+int calc_factorial(int num);
+
+int main()
+{
+	int value;
+
+	printf("Enter a positive integer: ");
+	fflush(stdout);
+	scanf("%d",&value);
+
+    // calculate factorial
+	printf("factorial of %d = %d", value , calc_factorial(value) );
+
+    return 0;
+}
+
+
+int calc_factorial(int num)
+{
+	if( num == 0 )
+		return 1;
+
+	return num*calc_factorial(num-1);
+}
