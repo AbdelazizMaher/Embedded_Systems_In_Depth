49379cb24a4544f751bc983a8bfcd7bfff10d2ec
ABDELAZIZ MAHER <abdelazizmaher17499@gmail.com>
2022-07-23 10:41:47 +0200

    commits

---
+++ Unit2_C_Programming/Lesson4_C_Arrays_& _strings_Assignments/Array_Assignment/EX3_Find_Find_Transpose_Of_Matrix.c
@@ -0,0 +1,59 @@
+/*
+ * main.c
+ *
+ *  Created on: July 20, 2022
+ *      Author: Abdelaziz Maher
+ */
+
+#include "stdio.h"
+
+int main()
+{
+	int Matrix[50][50];
+	int rows,columns;
+	int i,j;
+
+    printf("Enter rows and columns of a matrix : ");
+    fflush(stdout);
+    scanf("%d %d",&rows,&columns);
+
+    printf("Enter elements of Matrix :\n");
+    // Matrix
+    for(i=0 ; i<rows ; i++)
+    {
+    	for(j=0 ; j<columns ; j++)
+    	{
+    		printf("a%d%d : ", i+1 , j+1);
+    		fflush(stdout);
+    		scanf("%d",&Matrix[i][j]);
+    	}
+    }
+
+    printf("Entered Matrix :\n");
+    fflush(stdout);
+    // print Matrix
+    for(i=0 ; i<rows ; i++)
+    {
+    	for(j=0 ; j<columns ; j++)
+    	{
+    		printf("%d\t  ", Matrix[i][j]);
+    		fflush(stdout);
+    	}
+    	printf("\n");
+    }
+
+    printf("\nTranspose of Matrix :\n");
+    fflush(stdout);
+    // Transpose
+    for(i=0 ; i<columns ; i++)
+    {
+    	for(j=0 ; j<rows ; j++)
+    	{
+    		printf("%d\t  ", Matrix[j][i]);
+    		fflush(stdout);
+    	}
+    	printf("\n");
+    }
+
+    return 0;
+}
---
+++ Unit2_C_Programming/Lesson4_C_Arrays_& _strings_Assignments/Array_Assignment/EX4_Insert_An_Element_In_An_Array.c
@@ -0,0 +1,50 @@
+/*
+ * main.c
+ *
+ *  Created on: July 20, 2022
+ *      Author: Abdelaziz Maher
+ */
+
+#include "stdio.h"
+
+int main()
+{
+	int arr[50];
+	int size,element,Index,i,j;
+
+    printf("Enter no of elements : ");
+    fflush(stdout);fflush(stdin);
+    scanf("%d",&size);
+
+    // print array
+    for(i=1 ; i<=size ; i++)
+    {
+    	arr[i-1] = i;
+    	printf("%d\t  ", arr[i-1]);
+    	fflush(stdout);
+    }
+
+    printf("\nEnter the element to be inserted : ");
+    fflush(stdout);
+    scanf("%d",&element);
+
+    printf("\nEnter the location : ");
+    fflush(stdout);
+    scanf("%d",&Index);
+
+    // print array with inserted element
+    for(i=1 ; i<=(size+1) ; i++)
+    {
+    	if(i == Index)
+    	{
+    		// Reorganize
+    		for(j=size ; j>=Index ; j--)
+    			arr[j] = arr[j-1];
+    		// add element
+    		arr[i-1] = element;
+    	}
+    	printf("%d\t",arr[i-1]);
+    }
+
+    return 0;
+}
