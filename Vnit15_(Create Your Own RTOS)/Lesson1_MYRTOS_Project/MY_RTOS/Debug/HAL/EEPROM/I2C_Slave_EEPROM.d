4103745cb2747df53965d176f36ce288f94c6bfa
ABDELAZIZ MAHER <abdelazizmaher17499@gmail.com>
2023-12-16 13:21:24 +0200

    Part1

---
+++ Vnit15_(Create Your Own RTOS)/Lesson1_MYRTOS_Project/MY_RTOS/Debug/HAL/EEPROM/I2C_Slave_EEPROM.d
@@ -0,0 +1,11 @@
+HAL/EEPROM/I2C_Slave_EEPROM.o: ../HAL/EEPROM/I2C_Slave_EEPROM.c \
+ ../HAL/EEPROM/I2C_Slave_EEPROM.h \
+ C:/Users/Abdel/OneDrive/Documents/EmbeddedSystemsInDepth/STM32CUBE_IDE_Diploma_Workspace/Unit15_Create_Your_Own_RTOS/Create_Your_Own_RTOS/MY_RTOS/MCAL/Inc/Stm32f103c6_I2C_Driver.h \
+ C:/Users/Abdel/OneDrive/Documents/EmbeddedSystemsInDepth/STM32CUBE_IDE_Diploma_Workspace/Unit15_Create_Your_Own_RTOS/Create_Your_Own_RTOS/MY_RTOS/MCAL/Inc/Stm32f103x6.h \
+ C:/Users/Abdel/OneDrive/Documents/EmbeddedSystemsInDepth/STM32CUBE_IDE_Diploma_Workspace/Unit15_Create_Your_Own_RTOS/Create_Your_Own_RTOS/MY_RTOS/MCAL/Inc/Stm32f103c6_GPIO_Driver.h \
+ C:/Users/Abdel/OneDrive/Documents/EmbeddedSystemsInDepth/STM32CUBE_IDE_Diploma_Workspace/Unit15_Create_Your_Own_RTOS/Create_Your_Own_RTOS/MY_RTOS/MCAL/Inc/Stm32f103c6_RCC_Driver.h
+../HAL/EEPROM/I2C_Slave_EEPROM.h:
+C:/Users/Abdel/OneDrive/Documents/EmbeddedSystemsInDepth/STM32CUBE_IDE_Diploma_Workspace/Unit15_Create_Your_Own_RTOS/Create_Your_Own_RTOS/MY_RTOS/MCAL/Inc/Stm32f103c6_I2C_Driver.h:
+C:/Users/Abdel/OneDrive/Documents/EmbeddedSystemsInDepth/STM32CUBE_IDE_Diploma_Workspace/Unit15_Create_Your_Own_RTOS/Create_Your_Own_RTOS/MY_RTOS/MCAL/Inc/Stm32f103x6.h:
+C:/Users/Abdel/OneDrive/Documents/EmbeddedSystemsInDepth/STM32CUBE_IDE_Diploma_Workspace/Unit15_Create_Your_Own_RTOS/Create_Your_Own_RTOS/MY_RTOS/MCAL/Inc/Stm32f103c6_GPIO_Driver.h:
+C:/Users/Abdel/OneDrive/Documents/EmbeddedSystemsInDepth/STM32CUBE_IDE_Diploma_Workspace/Unit15_Create_Your_Own_RTOS/Create_Your_Own_RTOS/MY_RTOS/MCAL/Inc/Stm32f103c6_RCC_Driver.h:
---
+++ Vnit15_(Create Your Own RTOS)/Lesson1_MYRTOS_Project/MY_RTOS/Debug/MCAL/Src/Stm32f103c6_EXTI_Driver.d
@@ -0,0 +1,7 @@
+MCAL/Src/Stm32f103c6_EXTI_Driver.o: ../MCAL/Src/Stm32f103c6_EXTI_Driver.c \
+ C:/Users/Abdel/OneDrive/Documents/EmbeddedSystemsInDepth/STM32CUBE_IDE_Diploma_Workspace/Unit15_Create_Your_Own_RTOS/Create_Your_Own_RTOS/MY_RTOS/MCAL/Inc/Stm32f103c6_EXTI_Driver.h \
+ C:/Users/Abdel/OneDrive/Documents/EmbeddedSystemsInDepth/STM32CUBE_IDE_Diploma_Workspace/Unit15_Create_Your_Own_RTOS/Create_Your_Own_RTOS/MY_RTOS/MCAL/Inc/Stm32f103x6.h \
+ C:/Users/Abdel/OneDrive/Documents/EmbeddedSystemsInDepth/STM32CUBE_IDE_Diploma_Workspace/Unit15_Create_Your_Own_RTOS/Create_Your_Own_RTOS/MY_RTOS/MCAL/Inc/Stm32f103c6_GPIO_Driver.h
+C:/Users/Abdel/OneDrive/Documents/EmbeddedSystemsInDepth/STM32CUBE_IDE_Diploma_Workspace/Unit15_Create_Your_Own_RTOS/Create_Your_Own_RTOS/MY_RTOS/MCAL/Inc/Stm32f103c6_EXTI_Driver.h:
+C:/Users/Abdel/OneDrive/Documents/EmbeddedSystemsInDepth/STM32CUBE_IDE_Diploma_Workspace/Unit15_Create_Your_Own_RTOS/Create_Your_Own_RTOS/MY_RTOS/MCAL/Inc/Stm32f103x6.h:
+C:/Users/Abdel/OneDrive/Documents/EmbeddedSystemsInDepth/STM32CUBE_IDE_Diploma_Workspace/Unit15_Create_Your_Own_RTOS/Create_Your_Own_RTOS/MY_RTOS/MCAL/Inc/Stm32f103c6_GPIO_Driver.h:
